b4ad1d38ab105e4116a1fbdf628f69036c92d9a1
saitushaar <saitushaarr@gmail.com>
2023-05-31 22:16:25 +0530

    Final Project

--- scripts/post.js
+++ scripts/post.js
@@ -1,3 +1,31 @@
+// For adding comments
+function addComments(id) {
+    document.getElementById("viewComments").style.visibility = "visible";
+    var addEventName = id.value + "<br>";
+    var a = document.getElementById("userComments");
+    document.getElementById("addEventNames").innerHTML =
+        '<p style="padding:10px;background-color:white;">' +
+        addEventName +
+        "</p>" +
+        document.getElementById("addEventNames").innerHTML;
+    a.value = a.defaultValue;
+}
+//  For liked count
+var numberOfClicks = 0;
+function postLiked() {
+    document.getElementById("likeBlog").innerHTML =
+        `<i class="fa fa-thumbs-up" aria-hidden="true"></i>` + " Liked!";
+    numberOfClicks += 1;
+    if (numberOfClicks != 0) {
+        if (numberOfClicks == 1) {
+            document.getElementById("likeCount").innerHTML =
+                numberOfClicks + " person likes this!";
+        } else {
+            document.getElementById("likeCount").innerHTML =
+                numberOfClicks + " people have liked this!";
+        }
+    }
+}
 //  For edit button to change to save
 
 var editMode = false;
